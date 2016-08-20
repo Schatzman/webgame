3e976b8e755c1f20cc3784adff72aa21be8d5caf
Schatzman <devdan1984@gmail.com>
2016-08-19 22:16:02 -0600

    some updates

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# webgame
+# Webgame
+
+## Setup
+
+This app has been tested on CentOS 7 minimal install.
+
+To get started, run the following command:
+chmod +x setup.sh && ./setup.sh
+
+NOTE: You may need to edit the epel-release version in setup.sh if 7-8 is no longer available.
--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var app = express();
 
 app.use(express.static('static'));
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+function getRandInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+app.get('/poop/', function (req, res) {
+  res.send('Hello fuckerhead!' + getRandInt(1, 109342));
+});
 
 var port = 8080;
 
 app.listen(port, function () {
-  console.log('App listening on port ' + port + '!');
+  console.log('App listening on port ' + port + '! FUCKERFACE.');
 });
