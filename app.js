637b5cf3d45fa92b94aedd7580b1fe4159ed7cb9
Schatzman <devdan1984@gmail.com>
2016-08-27 11:25:08 -0700

    add example database function

--- app.js
+++ app.js
@@ -12,13 +12,33 @@ app.use(express.static("static"));
 var file = "test.db";
 var exists = fs.existsSync(file);
 
-var db = new sqlite3.Database(file);
-
 function getRandInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+var db = new sqlite3.Database(file);
+
+db.serialize(function(){
+
+    if (!exists) {
+        db.run("CREATE TABLE stuff (thing TEXT)");
+    }
+    var stmt = db.prepare("INSERT INTO stuff VALUES (?)");
+
+    for (i = 0; i < 10; i++) {
+        var rnd = getRandInt(500, 900000);
+        stmt.run("Thing #" + rnd);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, thing FROM Stuff", function(err, row) {
+        console.log(row.id + ": " + row.thing);
+    });
+
+    db.close();
+});
 
 app.get("/poop/", function (req, res) {
   res.send("Hello fuckerhead! " + getRandInt(1, 109342) + " shhhhooooooooooooot");
