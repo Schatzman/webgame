9324c682ffd3952e457802e7b20fd064ab94a1af
Schatzman <devdan1984@gmail.com>
2016-08-27 11:06:22 -0700

    add sqlite3 to imports and deps

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var express = require('express');
-var app = express();
+// *
+// DSM 2016
+// *
 
-app.use(express.static('static'));
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
+
+app.use(express.static("static"));
+
+var file = "test.db";
+var exists = fs.existsSync(file);
+
+var db = new sqlite3.Database(file);
 
 function getRandInt(min, max) {
   min = Math.ceil(min);
@@ -9,12 +20,12 @@ function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-app.get('/poop/', function (req, res) {
-  res.send('Hello fuckerhead!' + getRandInt(1, 109342));
+app.get("/poop/", function (req, res) {
+  res.send("Hello fuckerhead! " + getRandInt(1, 109342) + " shhhhooooooooooooot");
 });
 
 var port = 8080;
 
 app.listen(port, function () {
-  console.log('App listening on port ' + port + '! FUCKERFACE.');
+  console.log("App listening on port " + port + "! FUCKERFACE.");
 });
